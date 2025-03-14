b9d87ee9a50b151f5c3c0a68e9a672c7da3aa24e
Quivir Cutanda <quivir.c@codero.ph>
2025-03-14 17:17:44 +0800

    realtime date in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,28 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Avatar from "@/src/components/Avatar";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Header = () => {
+  const [currentDate, setCurrentDate] = useState("");
+
+  useEffect(() => {
+    const updateDate = () => {
+      const now = new Date();
+      const formattedDate = now.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+      });
+      setCurrentDate(formattedDate);
+    };
+
+    updateDate();
+    const interval = setInterval(updateDate, 60000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View className="flex-row items-center justify-between py-4 bg-secondary rounded-b-3xl shadow-lg shadow-black">
       <Avatar source={require("@/assets/Avatar/avatar.jpg")} size={34} />
@@ -11,7 +30,7 @@ const Header = () => {
         <Text className="text-lg font-bold text-primary">
           Hi, Quivir Cutanda
         </Text>
-        <Text className="text-base text-primary ">01 Jan 2024</Text>
+        <Text className="text-base text-primary">{currentDate}</Text>
       </View>
       <TouchableOpacity
         className="p-4"
