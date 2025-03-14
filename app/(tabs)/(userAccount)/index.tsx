c6900261d0d9218f1eeb42d94978ae8a4b996134
Quivir Cutanda <quivir.c@codero.ph>
2025-03-14 17:06:36 +0800

    Account-screen-UI

--- app/(tabs)/(userAccount)/index.tsx
+++ app/(tabs)/(userAccount)/index.tsx
@@ -1,12 +1,61 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import settingsData from "@/constant/data/settings.json";
+import Avatar from "@/src/components/Avatar";
+import SettingItem from "@/src/components/userAccount/userAccount";
 
-const account = () => {
+const Account = () => {
+  const [notifications, setNotifications] = useState(false);
+
+  const handleOnPress = (id: number) => {
+    switch (id) {
+      case 1:
+        console.log(1);
+        break;
+      case 2:
+        console.log(2);
+        break;
+      case 3:
+        !notifications ? setNotifications(true) : setNotifications(false);
+        break;
+      case 4:
+        console.log(4);
+        break;
+      case 5:
+        console.log(5);
+        break;
+    }
+  };
+
   return (
-    <View className="flex-1 justify-center items-center bg-primary">
-      <Text className="text-secondary text-lg">Coming soon..</Text>
+    <View className="flex-1 items-center gap-4">
+      <View className="bg-secondary w-full p-8 rounded-b-3xl items-center gap-4">
+        <Avatar size={80} source={require("@/assets/Avatar/avatar.jpg")} />
+
+        <Text className="text-lg font-bold text-primary text-center">
+          Quivir Cutanda
+        </Text>
+      </View>
+
+      <View className="w-full p-4 flex-1">
+        <Text className="text-base font-bold text-gray-400 py-4">
+          Preferences
+        </Text>
+        {settingsData.map(({ id, title, icon, isSwitch }) => (
+          <SettingItem
+            key={id}
+            title={title}
+            icon={icon}
+            isSwitch={isSwitch}
+            switchValue={notifications}
+            onSwitchToggle={setNotifications}
+            onPress={() => handleOnPress(id)}
+          />
+        ))}
+      </View>
     </View>
   );
 };
 
-export default account;
+export default Account;
