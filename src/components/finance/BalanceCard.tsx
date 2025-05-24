3deea34dea58b409ed8bcd565bf71ad0a95837d1
Quivir Cutanda <quivir.c@codero.ph>
2025-05-24 13:03:16 +0800

    make dynamic card in dashboard

--- src/components/finance/BalanceCard.tsx
+++ src/components/finance/BalanceCard.tsx
@@ -2,16 +2,21 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Entypo } from "@expo/vector-icons";
 
-const BalanceCard = () => {
+interface BalanceProp{
+  Remaining:number,
+  MonthlyIncome:number,
+  MonthlyExpenses:number
+}
+const BalanceCard = ({Remaining,MonthlyIncome,MonthlyExpenses}:BalanceProp) => {
   return (
     <View className="p-4 rounded-2xl bg-secondary flex-row justify-between items-center mt-4 shadow-lg shadow-black">
       <View flex-1>
-        <Text className="font-bold text-xl text-primary">₱ 8,000.00</Text>
-        <Text className="text-base font-normal text-primary">Remaining</Text>
-        <Text className="font-bold text-xl text-primary">₱ 800.00</Text>
-        <Text className="text-base font-normal text-primary">Budget</Text>
-        <Text className="font-bold text-xl text-primary">₱ 7000.56</Text>
-        <Text className="text-base font-normal text-primary">Expenses</Text>
+        <Text className="font-bold text-xl text-primary">₱ {Remaining!=null?Remaining.toFixed(2):"00.00"}</Text>
+        <Text className="text-base font-normal text-primary">Current Balance</Text>
+        <Text className="font-bold text-xl text-primary">₱ {MonthlyIncome!=null?MonthlyIncome.toFixed(2):"00.00"}</Text>
+        <Text className="text-base font-normal text-primary">Monthly Income</Text>
+        <Text className="font-bold text-xl text-primary">₱ {MonthlyExpenses!=null?MonthlyExpenses.toFixed(2):"00.00"}</Text>
+        <Text className="text-base font-normal text-primary">Monthly Expenses</Text>
       </View>
       <Entypo
         name="wallet"
